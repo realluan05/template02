5f94e9cfb2427f85829a7b26a102970af86e1dc3
Luan Henrique <luan.henrique.black@gmail.com>
2021-11-03 19:29:39 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<?php require_once 'template/head.php'; ?>
+<body>
+    <?php
+        require_once 'template/header.php';
+        require_once 'template/main.php';
+        require_once 'template/footer.php';
+    ?>
+</body>
+</html>
