39f5f6f497c4c355f1af003b2dd38397c241a6ee
Luan Henrique <luan.henrique.black@gmail.com>
2022-07-05 20:28:54 -0300

    alterando estrutura do projeto

---
+++ api/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once './template/head.phtml'; ?>
+<body>
+    <?php
+        require_once './template/header.phtml';
+        require_once './template/main.phtml';
+        require_once './template/footer.phtml';
+    ?>
+</body>
+</html>
